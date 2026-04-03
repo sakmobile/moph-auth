ea22ba820b495917e1e8372a07c44b1b72245bd3
sakmobile <sak.janenii@gmail.com>
2026-04-03 18:39:44 +0700

    init moph auth library update Fix config

--- config.php
+++ config.php
@@ -2,12 +2,12 @@
 
 return [
     'health_url' => 'https://moph.id.th',
-    'client_id' => '019240db-4fe4-7d09-8f30-bb4127279b9c', // ต้องตรงกับที่ตั้งใน Health ID
-    'client_secret' => '8d9b8ad092f37bd2b67a57aef73ad484c6a286e7', // ต้องตรงกับที่ตั้งใน Health ID
+    'client_id' => 'ClientID', // ต้องตรงกับที่ตั้งใน Health ID
+    'client_secret' => 'ClientSecret', // ต้องตรงกับที่ตั้งใน Health ID
     'redirect_uri' => 'http://localhost/moph-auth/examples/callback.php', // ต้องตรงกับที่ตั้งใน Health ID
 
     'provider_url' => 'https://provider.id.th',
-    'provider_client_id' => '0871858f-a5e6-429c-a53a-6c5a211e0175', // ต้องตรงกับที่ตั้งใน Provider ID
-    'provider_secret' => 'B89A4A635DC4E61C9E7FD49AC7319', // ต้องตรงกับที่ตั้งใน Provider ID
+    'provider_client_id' => 'ProviderClientID', // ต้องตรงกับที่ตั้งใน Provider ID
+    'provider_secret' => 'ProviderSecret', // ต้องตรงกับที่ตั้งใน Provider ID
 ];
 
